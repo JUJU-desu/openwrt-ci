f60b2a33fdac38e9372bc2314544d635e90f0993
JUJU-desu <218477375+JUJU-desu@users.noreply.github.com>
2025-12-30 16:30:57 +0800

    Update libwrt.sh

--- libwrt.sh
+++ libwrt.sh
@@ -3,6 +3,8 @@ rm -rf package/emortal/luci-app-athena-led
 # 修改默认IP
 sed -i 's/192.168.1.1/192.168.5.1/g' package/base-files/files/bin/config_generate
 
+# 移除luci-app-attendedsysupgrade软件包
+sed -i "/attendedsysupgrade/d" $(find ./feeds/luci/collections/ -type f -name "Makefile")
 
 # TTYD 免登录
 sed -i 's|/bin/login|/bin/login -f root|g' feeds/packages/utils/ttyd/files/ttyd.config
