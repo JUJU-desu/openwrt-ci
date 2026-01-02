ead40e4e7807397f333ccf0f232ae624a6e37037
JUJU-desu <218477375+JUJU-desu@users.noreply.github.com>
2026-01-03 00:43:39 +0800

    Update libwrt.sh

--- libwrt.sh
+++ libwrt.sh
@@ -8,3 +8,8 @@ sed -i "/attendedsysupgrade/d" $(find ./feeds/luci/collections/ -type f -name "M
 
 # TTYD 免登录
 sed -i 's|/bin/login|/bin/login -f root|g' feeds/packages/utils/ttyd/files/ttyd.config
+
+# 添加额外插件
+git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall-packages package/openwrt-passwall
+git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall package/luci-app-passwall
+git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall2 package/luci-app-passwall2
